3a7a6bdc1f59958563374640f9e0a907201b8d1e
imevanc <charal.vag@gmail.com>
2022-03-07 11:08:58 +0000

    updated cors

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ const {
   deleteCommentById,
   getApi,
 } = require("./controllers/news.controllers");
+
 const cors = require("cors");
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
